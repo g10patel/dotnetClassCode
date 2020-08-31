ea96f6c75e9cc87ce9266201d67660e4c6f45fd4
Jiten Patel <jitenpatel1013@gmail.com>
2020-08-30 20:09:25 -0500

    Initial commit

---
+++ src/GradeBook/Book.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+
+    public class Book{
+
+        
+
+        public Book(string name){
+            grades = new List<double>();
+            this.name = name;
+        }
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade);
+        }
+        public Statistics GetStatistics()
+        {
+            var result = new Statistics();
+            result.Average = 0.0;
+            result.High = double.MinValue;
+            result.Low = double.MaxValue;
+
+            foreach(var number in grades)
+            {
+                result.Low = Math.Min(number, result.Low);
+                result.High = Math.Max(number, result.High);
+                result.Average += number;
+            }
+
+            result.Average /= grades.Count;
+
+            return result;
+        }
+
+        private List<double> grades;
+
+        
+
+        private string name;
+
+    }
+
+}
